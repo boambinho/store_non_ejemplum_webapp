85b6c9fd3aeb2a354fc2611e004c4b7043ec59d4
Emilio <104663569+boambinho@users.noreply.github.com>
2023-12-19 01:23:05 -0300

    Footer base arreglado

---
+++ src/components/Footer.vue
@@ -0,0 +1,87 @@
+<script>
+
+</script>
+<template>
+    <div class="uk-container uk-padding uk-width-1-1 contenedor">
+        <div class="uk-grid uk-grid-column-small uk-grid-middle uk-text-center">
+            <div class="uk-width-1-3">
+
+                <p> Social media and Contact </p>
+                <ul class="uk-list uk-flex uk-flex-column uk-flex-middle">
+                    <li class="uk-margin-small-bottom">
+                        <a href="#">
+                            <i class="fa-brands fa-instagram fa-lg uk-margin-small-right"></i>
+                            Instagram
+                        </a>
+                    </li>
+                    <li class="uk-margin-small-bottom">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook fa-lg uk-margin-small-right"></i>
+                            Facebook
+                        </a>
+                    </li>
+                    <li class="uk-margin-small-bottom">
+                    <a href="#">
+                        <i class="fa-solid fa-phone fa-lg uk-margin-small-right"></i>
+                        +569 6344 5145
+                    </a>
+                    </li>
+                    <li class="uk-margin-small-bottom">
+                    <a href="#">
+                        <i class="fa-regular fa-envelope fa-lg uk-margin-small-right"></i>
+                        E-mail
+                    </a>
+                    </li>
+                </ul>
+
+            </div>
+
+
+            <div class="uk-width-1-3 uk-flex-1">
+                <p> Categories </p>
+                <ul class="uk-list">
+                    <li> <a href="#"> Offers</a> </li>
+                    <li> <a href="#"> Shoes </a> </li>
+                    <li> <a href="#"> Pants </a> </li>
+                    <li> <a href="#"> Shirts </a> </li>
+                    <li> <a href="#"> Sweatshirts </a> </li>
+
+                </ul>
+            </div>
+
+            <div class="uk-width-1-3">
+                <p> Design </p>
+                <ul class="uk-list">
+                    <li> <a href="#"> Offers</a> </li>
+                    <li> <a href="#"> Shoes </a> </li>
+                    <li> <a href="#"> Pants </a> </li>
+                    <li> <a href="#"> Shirts </a> </li>
+                    <li> <a href="#"> Sweatshirts </a> </li>
+
+                </ul>
+            </div>
+
+            <div class="uk-width-1 uk-flex-middle uk-margin-large-top">
+
+                <p>
+                    © 2023 Emilio Bustos. All rights reserved.
+                </p>
+
+            </div>
+
+            <div class="uk-width 1-2 uk-margin-medium-top">
+
+                <a href="#" uk-totop> </a>
+            </div>
+
+
+        </div>
+
+    </div>
+</template>
+
+<style>
+.contenedor {
+    background-color: rgba(185, 186, 186, 0.21);
+}
+</style>
